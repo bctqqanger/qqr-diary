9b54b58737fc05f742e4a8adf0e0cf8315d409e6
aaqbbc <317679615@qq.com>
2026-09-19 02:40:50 +0800

    更新：PC端首页仅展示形象照，点击后展开照片墙；放大层不再放大视频

--- assets/js/main.js
+++ assets/js/main.js
@@ -605,6 +605,16 @@
     if (/^#\/friend\//.test(location.hash)) location.hash = '';
   });
 
+  // PC 端「✕ 收起」：与窄屏返回一致，退回单页形象照
+  var closeBtn = document.getElementById('closeBtn');
+  if (closeBtn) {
+    closeBtn.addEventListener('click', function () {
+      pauseAllVideos();
+      document.body.classList.remove('is-detail');
+      if (/^#\/friend\//.test(location.hash)) location.hash = '';
+    });
+  }
+
   /* -------------------------------------------------------- 启动 */
 
   window.addEventListener('hashchange', routeFromHash);
